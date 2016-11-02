ec1231ad063a59fbc6279fff04996f0cd0b0c98e
wychi <cwenyuan@gmail.com>
2016-11-03 02:15:22 +0800

    Update ReverseLinkedList_WY.java

--- 206_Reverse-Linked-List/ReverseLinkedList_WY.java
+++ 206_Reverse-Linked-List/ReverseLinkedList_WY.java
@@ -1,3 +1,7 @@
+/**
+檢討：
+Ln7 的邊界條件檢查是多餘的，可以被 Ln13的 while-loop 檢查取代
+ */
 public class Solution {
     public ListNode reverseList(ListNode head) {
         if(head == null || head.next == null) return head;
