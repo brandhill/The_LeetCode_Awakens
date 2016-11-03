f1a8db3be2d516d37bcb22ad37975eea8326a7b9
Hill Hung <brandhill.g@gmail.com>
2016-11-03 21:37:22 +0800

    add ReverseLinkedList

---
+++ 206_Reverse-Linked-List/ReverseLinkedList_Hill.java
@@ -0,0 +1,30 @@
+/**
+ * Created by hill on 2016/11/3.
+ * <p>
+ * http://www.geeksforgeeks.org/write-a-function-to-reverse-the-nodes-of-a-linked-list/
+ * <p>
+ * Your runtime beats 31.80% of java submissions.
+ * <p>
+ * <p>
+ * A linked list can be reversed either iteratively or recursively. Could you implement both?
+ */
+public class ReverseLinkedList {
+
+
+    public static ListNode reverseList(ListNode head) {
+
+        ListNode pre = null;
+        ListNode cur = head;
+        ListNode next = null;
+
+        while (cur != null) {
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+
+        head = pre;
+        return head;
+    }
+}
