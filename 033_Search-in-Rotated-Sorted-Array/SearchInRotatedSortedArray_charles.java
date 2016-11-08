13624facebd7dba4f547c28248701d09101f5077
Charles Tsai <charles.tsai@cmcm.com>
2016-11-08 12:28:57 +0800

    update 033

--- 033_Search-in-Rotated-Sorted-Array/SearchInRotatedSortedArray_charles.java
+++ 033_Search-in-Rotated-Sorted-Array/SearchInRotatedSortedArray_charles.java
@@ -1,25 +1,49 @@
 public class Solution {
-
+    private int largestIndex;
     public int search(int[] nums, int target) {
+        largestIndex = nums.length - 1;
+        int[] array = reorderArray(nums);
+        int result = binarySearch(array, 0, array.length, target);
+        if (result < 0) {
+            return -1;
+        } else {
+            return (largestIndex + result + 1)%array.length;
+        }
+    }
+
+    private int[] reorderArray(int[] nums) {
+        int[] result = new int[nums.length];
+        largestIndex = nums.length - 1;
+        for (int i = 0; i < nums.length - 1; ++i) {
+            if (nums[i + 1] < nums [i]) {
+                largestIndex = i;
+                break;
+            }
+        }
 
         for (int i = 0; i < nums.length; ++i) {
-
+            result[i] = nums[(i + largestIndex + 1)%nums.length];
         }
 
+        return result;
     }
 
-    int binarySearch(int[] nums, int target) {
+    private static int binarySearch(int[] nums, int begin, int end, int target) {
         int index = begin + (end - begin) / 2;
-
-        if (target == nums[index]) {
-            System.out.println("got target!!!");
-            return index;
-        } else if (target > nums[index]) {
-            System.out.println("searching " + (begin + (end - begin)/2 + 1) + " to " + end);
-            return binarySearch(nums, begin + (end - begin)/2 + 1, end, target);
-        } else {
-            System.out.println("searching " + begin + " to " + (begin + (end - begin)/2 - 1));
-            return binarySearch(nums, begin, begin + (end - begin)/2 - 1, target);
+        if (begin > end) {
+            return -1; 
         }
-    }
+
+        try {
+            if (target == nums[index]) {
+                return index;
+            } else if (target > nums[index]) {
+                return binarySearch(nums, begin + (end - begin)/2 + 1, end, target);
+            } else {
+                return binarySearch(nums, begin, begin + (end - begin)/2 - 1, target);
+            }
+        } catch (Exception e) {
+            return -1;
+        }
+    }   
 }
