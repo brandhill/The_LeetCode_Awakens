8bf656b7147c93894b5c7778990ab022d5eb23fa
Johnny Lee <naje0522@gmail.com>
2016-11-09 01:24:17 +0800

    Minor update

--- 173_Binary-Search-Tree-Iterator/BinarySearchIterator_Johnny.java
+++ 173_Binary-Search-Tree-Iterator/BinarySearchIterator_Johnny.java
@@ -1,8 +1,9 @@
 /*
  * Author: Johnny Lee
- * Time Complexity: Average O(n)
+ * Time Complexity: Average O(1)
  * Space Complexity: O(h)
  * Question1: 已跪...應該沒別招了
+ * 補充： stack內建synchornize會比linkedlist慢
  */
 public class BSTIterator {
     private Stack<TreeNode> mStack = new Stack();
