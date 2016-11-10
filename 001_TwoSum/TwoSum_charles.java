26d2f62dc49f5073a6ccacacd7ab3002d4fdbb99
Charles Tsai <charles.tsai@cmcm.com>
2016-11-10 17:28:59 +0800

    Add 001 solution.

---
+++ 001_TwoSum/TwoSum_charles.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; ++i) {
+            for (int j = nums.length - 1; j > i; --j) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
