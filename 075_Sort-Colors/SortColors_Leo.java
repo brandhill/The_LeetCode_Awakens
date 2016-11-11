5fc8a25da6234fa08d4d97e7f9e1391eeb648470
leareai <eai.lear@gmail.com>
2016-11-11 23:47:48 +0800

    Create SortColors_Leo.java

---
+++ 075_Sort-Colors/SortColors_Leo.java
@@ -0,0 +1,25 @@
+/*
+ *Use counting sort, its advantage is easy to extend if we have more colors.
+ *Time : O(n)
+ *Space: O(k)
+ */
+
+public class Solution {
+  private static final int COLOR_NUMBER = 3;
+  public void sortColors(int[] nums) {
+    int[] colorSet = new int[COLOR_NUMBER];
+    for (int v : nums) {
+      colorSet[v]++;
+    }
+   
+    int startIndex = 0;
+    int number = 0;
+    for (int i = 0; i < COLOR_NUMBER; i++) {
+      number += colorSet[i];
+      for (int j = startIndex; j < number; j++) {
+        nums[j] = i;
+      }
+      startIndex = number;
+    }
+  }
+}
