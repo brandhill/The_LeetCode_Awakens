273f9a1c36b18daa266a04fe137bbcdc493201ae
leareai <eai.lear@gmail.com>
2016-11-11 18:18:18 +0800

    Update solution to an executable format

--- MinimalRunTimeScheduler/MinimalRunTtimeScheduler_with_limit_input.java
+++ MinimalRunTimeScheduler/MinimalRunTtimeScheduler_with_limit_input.java
@@ -1,8 +1,47 @@
 class Solution {
+    public static void log(List<List<Integer>> lists) {
+        for (List<Integer> list:lists) {
+            StringBuilder sb = new StringBuilder();
+            for (Object o:list) {
+                if (sb.length() > 0) sb.append(',');
+                sb.append(o.toString());
+            }
+            log(sb.toString());
+        }
+    }
+    
+    public static void log(String s) {
+        System.out.println(s);
+    }
+  
+  private int getTasksCost(String tasks) {
+    char prev = tasks.charAt(0); 
+    int timeCost = 1;
+    for (int i = 1, size = tasks.length(); i < size; i++) {
+      char current = tasks.charAt(i); 
+      if (current == prev) {
+        timeCost += 3; 
+      }
+      prev = current; 
+    }
+    return timeCost;
+  }
+
+
+  public static class TaskSequence {
+    String tasks;
+    int timeCost;
+    public String toString() {
+      return "task:"+tasks+", time:"+timeCost;
+    }
+  }
+  
   private static final int TYPES = 26;
-  public int calculateMinTasksCost(String tasks) {
+  public TaskSequence calculateMinTasksCost(String tasks) {
+    TaskSequence ret = new TaskSequence();
+    if (null == tasks) return ret;
     int taskSize = tasks.length();
-    if (null == tasks || taskSize == 0) return 0;
+    if (taskSize == 0) return ret;
     int timeCost = 0;
 
     int[] numOfTask = new int[TYPES];
@@ -45,7 +84,16 @@ class Solution {
         timeCost++;
         sb.append(c);
     }
+    ret.tasks = sb.toString();
+    ret.timeCost = timeCost;
+    return ret;
+  }
+
+    
   
-    return timeCost;
+  public static void main(String[] args) {
+      Solution s = new Solution();
+      String input = "AAAAAAABB";
+      log("ret:"+s.calculateMinTasksCost(input));
   }
 }
