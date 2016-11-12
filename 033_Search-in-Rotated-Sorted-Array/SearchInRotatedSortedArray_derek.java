db7ff726ed8bbf26bb25e6299e7e15d088257e5c
Derek Lai <derek.lai@ileopard.com>
2016-11-13 01:44:27 +0800

    Add SearchInRotatedSortedArray

--- 033_Search-in-Rotated-Sorted-Array/SearchInRotatedSortedArray_derek.java
+++ 033_Search-in-Rotated-Sorted-Array/SearchInRotatedSortedArray_derek.java
@@ -2,70 +2,113 @@
  * Author: Derek Lai
  * Question 1: Empty array ?
  * Question 2: DESC ? ASEC ?
- * 
- * Time: O(n)
+ *
+ * Time: O(log n)
  * Space:O(1)
- */ 
+ */
 public class Solution {
+    
     /**
-     * Solution:Binary Search
+     * Solution:Binary Search v2
      * Time: O(log n)
      * Space:
-     * 
-     * Runtime: 21 ms
-     */ 
+     *
+     * Runtime: 13 ms
+     */
     public int search(int[] nums, int target) {
         final int length = null != nums ? nums.length : 0;
         if (length <= 0) {
             return -1;
         }
         
-        //Find the lowest index
-        int start = 0, end = length-1;
-        int midIdx = 0;
+        // Find the smallest index
+        int start = 0, end = length -1;
+        int minIdx = 0;
         while (start < end) {
-            midIdx = (start+end) / 2;
-            if (nums[midIdx] > nums[end]) {
-                start = midIdx + 1;
+            minIdx = start + (end - start) / 2;
+            if (nums[minIdx] > nums[end]) {
+                start = minIdx + 1;
             } else {
-                end = midIdx;
+                end = minIdx;
+            }
+        }
+        minIdx = start;
+        
+        start = target <= nums[length - 1] ? minIdx : 0;
+        end = target > nums[length - 1] ? minIdx : length - 1;
+        while (start <= end) {
+            minIdx = start + (end - start) / 2;
+            if (nums[minIdx] == target) {
+                return minIdx;
+            } else if (nums[minIdx] > target) {
+                end = minIdx - 1 ;
+            } else {
+                start = minIdx + 1;
             }
         }
         
-        //if start == end, that is rotated index
-        int rotatedIdx = start;
-        int realMidIdx = 0;
-        start = 0;
-        end = length-1;
-        System.out.println("rotataedIdx:"+rotatedIdx);
-        while (start <= end) {
-            midIdx = (start+end) / 2;
-            realMidIdx = (midIdx+rotatedIdx) % length;
-            if (nums[realMidIdx] == target) {
-                return realMidIdx;
-            } else if (nums[realMidIdx] < target) {
-                start = midIdx + 1;
-            } else {
-                end = midIdx - 1;
-            }
-        }
-
         return -1;
     }
     
     /**
-     * Solution: Iterator
-     * Time: O(n)
-     * Space:O(1)
-     * 
-     * Runtime: 18 ms
-     */ 
+     * Solution:Binary Search v1
+     * Time: O(log n)
+     * Space:
+     *
+     * Runtime: 21 ms
+     */
     // public int search(int[] nums, int target) {
     //     final int length = null != nums ? nums.length : 0;
     //     if (length <= 0) {
     //         return -1;
     //     }
-        
+    
+    //     //Find the lowest index
+    //     int start = 0, end = length-1;
+    //     int midIdx = 0;
+    //     while (start < end) {
+    //         midIdx = (start+end) / 2;
+    //         if (nums[midIdx] > nums[end]) {
+    //             start = midIdx + 1;
+    //         } else {
+    //             end = midIdx;
+    //         }
+    //     }
+    
+    //     //if start == end, that is rotated index
+    //     int rotatedIdx = start;
+    //     int realMidIdx = 0;
+    
+    //     start = 0;
+    //     end = length-1;
+    //     while (start <= end) {
+    //         midIdx = (start+end) / 2;
+    //         realMidIdx = (midIdx+rotatedIdx) % length;
+    //         if (nums[realMidIdx] == target) {
+    //             return realMidIdx;
+    //         } else if (nums[realMidIdx] < target) {
+    //             start = midIdx + 1;
+    //         } else {
+    //             end = midIdx - 1;
+    //         }
+    //     }
+    
+    //     return -1;
+    // }
+    
+    /**
+     * Solution: Iterator
+     * Time: O(n)
+     * Space:O(1)
+     *
+     * Runtime: 18 ms
+     */
+    // public int search(int[] nums, int target) {
+    //     final int length = null != nums ? nums.length : 0;
+    //     if (length <= 0) {
+    //         return -1;
+    //     }
+    
     //     int n = -1;
     //     if (nums[0] == target) {
     //         return 0;
