0535930818d98ab76cd57c507206f945dbddb8d8
leareai <eai.lear@gmail.com>
2016-11-13 03:36:58 +0800

    Create SearchInRotatedSortedArray_leo.java

---
+++ 033_Search-in-Rotated-Sorted-Array/SearchInRotatedSortedArray_leo.java
@@ -0,0 +1,66 @@
+/*
+ *  Time: O(log(n));
+ * Space: O(1);
+ */
+public class Solution {
+      private int getRotatedPin(int [] nums) {
+        // index: 0, 1, 2, 3, 4, 5, 6, 7
+        // value: 3, 4, 5, 6, 7, 0, 1, 2, => offset=5
+        // the pin point satisfies that p[i-1] > p[i] where i-1 < i;
+        int size = nums.length;
+        int l = 0;
+        int h = size-1;
+        int m = 0;
+        while (l < h) {
+          m = (l+h)/2;
+          int prevM = 0 == m ? size-1: m -1;
+          if (nums[prevM]> nums[m]) return m;
+        // index: 0, 1, 2, 3, 4, 5, 6, 7
+        // value: 3, 4, 5, 6, 7, 0, 1, 2, => offset=5
+        //        l        m           h  
+          if (nums[h] < nums[m]) {
+            l = m + 1;
+          } else {
+            h = m -1;
+          }
+        }
+        // index: 0, 1, 2, 3, 4, 5, 6, 7
+        // value: 1, 2, 3, 4, 5, 6, 7, 0, offset=0
+        
+        return l;
+      }
+      
+      private int getRealIndex(int index, int offset, int size) {
+        int ret = index+offset;
+        if (ret >= size) {
+          return ret - size;
+        } else if (ret < 0){
+          return ret + size;
+        }
+        return ret;
+      }
+      
+      private int searhWithOffset(int nums[], int target, int offset) {
+        int size = nums.length;
+        int l =0;
+        int h = size;
+        while (l <= h) {
+          int m = (l+h)/2;
+          int indexM = getRealIndex(m, offset, size);
+          int value = nums[indexM];
+          if (value > target) {
+            h = m - 1;
+          } else if (value < target) {
+            l = m + 1;
+          } else {
+            return indexM;
+          }
+        }
+        return -1;
+      }
+      
+      public int search(int[] nums, int target) {
+        int offset = getRotatedPin(nums);
+        return searhWithOffset(nums, target, offset);
+      }
+}
