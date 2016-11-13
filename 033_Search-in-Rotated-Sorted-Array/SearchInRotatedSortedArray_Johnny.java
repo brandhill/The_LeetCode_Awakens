4bec29affe1cde98235909aa20bcdf1e28cf400f
Johnny Lee <naje0522@gmail.com>
2016-11-14 00:49:49 +0800

    Add solution 33

--- 033_Search-in-Rotated-Sorted-Array/SearchInRotatedSortedArray_Johnny.java
+++ 033_Search-in-Rotated-Sorted-Array/SearchInRotatedSortedArray_Johnny.java
@@ -1,38 +1,48 @@
-/*
-  Auther: Johnny Lee
-  Time complexicity: O(logn)
-  Space complexitty: O(1)
+/* 
+ * Time complexity: O(logn)
+ * Space complexity: O(1)
  */
-
 public class Solution {
     public int search(int[] nums, int target) {
-        int l = 0;
-        int r = nums.length - 1;
+        int left = 0;
+        int right = nums.length - 1;
         
-        while (l <= r) {
-            int m = (l + r) / 2;
-            if (target == nums[m]) {
-                return m;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
             }
             
-            if (nums[l] <= nums[m]) {
-                // left sorted
-                if (target >= nums[l] && target < nums[m]) {
-                    // target is on the left
-                    r = m - 1;
+            if (nums[mid] > nums[left] && nums[mid] < nums[right]) {
+                // didn't rotated
+                if (nums[mid] < target) {
+                    // move right
+                    left = mid + 1;
                 } else {
-                    // target is on the right
-                    l = m + 1;
+                    // move left
+                    right = mid - 1;
                 }
+            } else if (nums[mid] > nums[left]) {
+                    // left side is ordered
+                    if (nums[mid] > target && nums[left] <= target) {
+                        // move left
+                        right = mid - 1;
+                    } else {
+                        // move right
+                        left = mid + 1;
+                    }
+            } else if (nums[mid] < nums[right]) {
+                    // right side is ordered
+                    if (nums[mid] < target && nums[right] >= target) {
+                        // move right
+                        left = mid + 1;
+                    } else {
+                        // move left
+                        right = mid - 1;
+                    }
             } else {
-                // right sorted
-                if (target > nums[m] && target <= nums[r]) {
-                    // target is on the right
-                    l = m + 1;
-                } else {
-                    // target is on the left
-                    r = m - 1;                    
-                }
+                // move left
+                left = mid + 1;
             }
         }
         
