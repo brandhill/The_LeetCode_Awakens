c1472c72159b997f99f165c374eab416bf063200
leareai <eai.lear@gmail.com>
2016-11-14 02:15:27 +0800

    Add another way to solve this questions

--- 033_Search-in-Rotated-Sorted-Array/SearchInRotatedSortedArray_leo.java
+++ 033_Search-in-Rotated-Sorted-Array/SearchInRotatedSortedArray_leo.java
@@ -63,4 +63,29 @@ public class Solution {
         int offset = getRotatedPin(nums);
         return searhWithOffset(nums, target, offset);
       }
+      
+  public int searchByCheckingConsistency(int[] nums, int target) {
+    int l = 0, h = nums.length-1;
+    int first = nums[0];
+    while (l <= h) {
+        int m = (l + h) / 2;
+        int mV = nums[m];
+        if (mV < first && target >= first) {
+          h = m - 1;
+          continue;
+        } else if (mV >= first && target < first) {
+          l = m + 1;
+          continue;
+        }
+                   
+        if (mV < target) {
+            l = m + 1;
+        } else if (mV > target) {
+            h = m - 1;
+        } else {
+            return m;
+        }
+    }
+    return -1;
+  }
 }
