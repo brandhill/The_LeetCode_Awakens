53d44114a07495bbdbc5387941c89d185d9acb41
Charles Tsai <charles.tsai@cmcm.com>
2016-11-14 14:29:54 +0800

    Add alternative solution for 075.

--- 075_Sort-Colors/SortColors_charles.java
+++ 075_Sort-Colors/SortColors_charles.java
@@ -4,6 +4,9 @@
  *		It's a 1-pass sorting algorithm and quite intuitive. Iterate all elements and check if it's the color we expect.
  *		If no, find the first right color and exchange with it. If cannot find right color, it means that current color is exhausted.
  *		Therefore we can start to try next color for the sorting.
+ *
+ *      Time complexity: O(n^2)
+ *      Space complexity: O(1)
  */
 public class Solution {
     public void sortColors(int[] nums) {
@@ -31,5 +34,38 @@ public class Solution {
             }
         }
         return false;
+    }
+}
+
+/*
+ *      If we follow the hint for this problem, we can have a 2-pass counting sort to accomplish this task.
+ *
+ *      Time complexity: O(n)
+ *      Space complexity: O(m), m is the number of colors
+ */
+
+public void sortColors(int[] nums) {
+    //count number of three colors
+    int numOfRed = 0;
+    int numOfBlue = 0;
+    int numOfWhite = 0;
+    if (nums.length == 0 || nums == null) return;
+
+    //count red
+    for (int i : nums) {
+        if (i == 0) numOfRed++;
+    }
+    for (int i : nums) {//count white
+        if (i == 1) numOfWhite++;
+    }
+    numOfBlue = nums.length - numOfRed - numOfWhite;
+    for (int i = 0; i < numOfRed; i++) {
+        nums[i] = 0;
+    }
+    for (int i = numOfRed; i < numOfRed + numOfWhite; i++) {
+        nums[i] = 1;
+    }
+    for (int i = numOfRed + numOfWhite; i < nums.length; i++) {
+        nums[i] = 2;
     }
 }
