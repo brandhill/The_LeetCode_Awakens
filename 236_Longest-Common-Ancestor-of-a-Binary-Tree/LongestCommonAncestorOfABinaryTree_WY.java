2cde276650572c711950df74643ad7d03d191850
wychi <cwenyuan@gmail.com>
2016-11-18 23:03:20 +0800

    Create LongestCommonAncestorOfABinaryTree_WY.java

---
+++ 236_Longest-Common-Ancestor-of-a-Binary-Tree/LongestCommonAncestorOfABinaryTree_WY.java
@@ -0,0 +1,41 @@
+/**
+# Question
+* Does this tree contain duplicate values?
+
+# thoughs
+if node n is the common ancestor, one of those conditions should be true.
+1. find p and q in node's subtree. one in left and the other in the right.
+2. node n is p or q, and find the other on in n's subtree.
+
+we can do a **post-order** tree traversal to find p and q.
+during the traversal, we will found the a node fulfill the conditions.
+
+> **pre-order** makes code much compact
+> A modified version of pre-order traversal. The point to understand this is, once a sub-branch has a possible ancestor, all its super branches will have the same one.
+
+# Complexity
+Time : n
+Space: logn
+
+ */
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // pre-order
+        if(root == null || root == p || root == q)
+            return root;
+
+        TreeNode leftResult = lowestCommonAncestor(root.left, p, q);
+        TreeNode rightResult = lowestCommonAncestor(root.right, p, q);
+
+        if(leftResult != null && rightResult != null) {
+            return root;
+        }
+        
+        // post-order
+        //if(root == p || root == q) {
+        //    return root;
+        //}
+        
+        return leftResult != null ? leftResult : rightResult;
+    }
+}
