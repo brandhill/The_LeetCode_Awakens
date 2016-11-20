db4cd24bab697058000780d85a817365454eb7e9
Hill <brandhill.g@gmail.com>
2016-11-21 01:13:01 +0800

    add Kth Largest Element in an Array

---
+++ 215_Kth-Largest-Element-in-an-Array/KthLargestElementinanArray_Hill.java
@@ -0,0 +1,47 @@
+/*
+https://leetcode.com/problems/kth-largest-element-in-an-array/
+
+http://pages.cs.wisc.edu/~vernon/cs367/notes/11.PRIORITY-Q.html
+ */
+
+
+public class Solution {
+    /**
+     * Time  :O( nLogn)
+     * Space :O(1)
+     */
+    public int findKthLargest1(int[] nums, int k) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        return nums[n - k];
+    }
+
+
+    /**
+     * loop through the arrry, and put each of element in to a max-heap, whose size is k
+     *
+     *
+     * Time  :O(n logn)
+     * Space :O(1)
+     */
+
+    public int findKthLargest(int[] nums, int k) {
+        if(nums.length < k){
+            return 0;
+        }
+        Queue<Integer> pq = new PriorityQueue<Integer>(k);
+        for(int i = 0; i < nums.length; i++){
+            if(i < k){
+                pq.add(nums[i]);
+            }else{
+                if(nums[i] > pq.peek()){
+                    pq.poll();
+                    pq.add(nums[i]);
+                }
+            }
+        }
+        return pq.peek();
+
+    }
+
+}
