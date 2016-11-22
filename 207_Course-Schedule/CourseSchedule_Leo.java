9e88f6bb10f44fcbb193abec4677c54556e6e54f
leareai <eai.lear@gmail.com>
2016-11-23 03:51:26 +0800

    Change the graph representation
    To improve performance

--- 207_Course-Schedule/CourseSchedule_Leo.java
+++ 207_Course-Schedule/CourseSchedule_Leo.java
@@ -1,14 +1,17 @@
 public class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         int[] indegree = new int[numCourses];
-        int[][] graph = new int[numCourses][numCourses];
+        ArrayList<Integer>[] graph = new ArrayList[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            graph[i] = new ArrayList<>();
+        }
         
         for (int [] dependancy: prerequisites) {
             int to = dependancy[1];
             int from = dependancy[0];
-            if (graph[from][to] == 0) {
+            if (!graph[from].contains(to)) {
                 indegree[to]++;
-                graph[from][to] = 1;
+                graph[from].add(to);
             }
         }
         
@@ -19,12 +22,9 @@ public class Solution {
         }
         int count = 0;
         while (!queue.isEmpty()) {
-            int from = queue.removeFirst();
             count++;
-            for (int i = 0; i < numCourses; i++) {
-                if (graph[from][i] != 0) {
-                    if (--indegree[i] == 0) queue.add(i);
-                }
+            for (int i : graph[queue.removeFirst()]) {
+                if (--indegree[i] == 0) queue.add(i);
             }
         }
 
