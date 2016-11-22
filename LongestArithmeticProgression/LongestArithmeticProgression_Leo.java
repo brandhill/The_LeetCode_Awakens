355a41318a6a7008dd33a631f2ca4bc84eda877c
leareai <eai.lear@gmail.com>
2016-11-23 01:37:56 +0800

    Add dp solution

--- LongestArithmeticProgression/LongestArithmeticProgression_Leo.java
+++ LongestArithmeticProgression/LongestArithmeticProgression_Leo.java
@@ -7,6 +7,38 @@ class GFG {
         System.out.println(s);
     }
     
+    public static int llapDp(int[] arr, int size) {
+      if (size <= 2) return size;
+      int[][] dp = new int[size][size];
+      int lastIndex = size - 1;
+      for (int i = 0; i < size; i++) {
+          dp[i][lastIndex] = 2;
+      }
+      
+      int max = 2;
+      for (int j = lastIndex-1; j > 0; j--) {
+          int i = j - 1;
+          int k = j + 1;
+          while (i >= 0 && k < size) {
+              int sum = arr[k]+arr[i];
+              int target = arr[j]<<1;
+              if (sum > target) {
+                  dp[i--][j] = 2;
+              } else if (sum < target){
+                  k++;
+              } else {
+                  dp[i][j] = dp[j][k]+1;
+                  if (dp[i][j] > max) max = dp[i][j];
+                  i--;
+                  k++;
+              }
+          }
+          while (i>=0) {
+              dp[i--][j] = 2;
+          }
+      }
+      return max;
+  }
     
   public static int llap(int[] arr, int size) {
     int max = 1;
