fceec1035242a20367536285525882dee65fafb7
Charles Tsai <charles.cp.tsai@gmail.com>
2016-11-23 01:14:26 +0800

    Update 005 DP solution.

--- 005_Longest-Palindromic-Substring/LongestPalindromicSubstring_charles.java
+++ 005_Longest-Palindromic-Substring/LongestPalindromicSubstring_charles.java
@@ -1,3 +1,10 @@
+/*
+	Author: Charles Tsai
+	
+	My solution  is trying to traverse the center of palindrome sequentially. Once traverse to a character, check if it's left and rigtht one is equivalent or not, and then increment the count.
+	Note that we should split odd and even case respectively.
+	
+*/
 public class Solution {
     public String longestPalindrome(String s) {
         if (s == null) throw new IllegalArgumentException("input is null");
@@ -54,5 +61,63 @@ public class Solution {
         //System.out.println("p=" + palindromeIndex + " " + charArray[palindromeIndex] + ", m=" + maxLength + " b=" + beginIndex + " e=" + endIndex);
 
         return s.substring(beginIndex, endIndex);
+    }
+}
+
+
+/*
+	Use DP to solve this. It can be initialized with length = 1 and length = 2.
+ */
+ public class Solution {
+    public String longestPalindrome(String s) {
+        
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        
+        int max = 1;
+        int start = 0, end = 0;
+        
+        for(int i = 0; i < n; i++)
+        {
+            dp[i][i] = true;
+        }
+        
+        for(int i = 0; i < n - 1; i++)
+        {
+            if(s.charAt(i) == s.charAt(i + 1))
+            {
+                dp[i][i + 1] = true;
+                max = 2;
+                start = i;
+                end = i + 1;
+            }
+            else
+            {
+                dp[i][i + 1] = false;
+            }
+        }
+        
+        for(int k = 3; k <= n; k++)
+        {
+            for(int i = 0; i < n - k + 1; i++)
+            {
+                int j = i + k - 1;
+                
+                dp[i][j] = dp[i + 1][j - 1] && (s.charAt(i) == s.charAt(j));
+                if(dp[i][j])
+                {
+                    if(k > max)
+                    {
+                        max = k;
+                        start = i;
+                        end = j;
+                    }
+                }
+                
+            }
+        }
+        
+        return s.substring(start, end + 1);
+        
     }
 }
