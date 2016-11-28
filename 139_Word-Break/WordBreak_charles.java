85aada220093d622ec867e84bb927be96272b253
Charles Tsai <charles.tsai@cmcm.com>
2016-11-28 20:33:55 +0800

    Update 139 DP solution.

--- 139_Word-Break/WordBreak_charles.java
+++ 139_Word-Break/WordBreak_charles.java
@@ -30,3 +30,23 @@ public class Solution {
     }
 }
 
+/*
+    DP solution to make task complete faster.
+
+*/
+public class Solution {
+
+    public boolean wordBreak(String s, Set<String> wordDict) {
+        int len = s.length();
+        boolean[] f = new boolean[len+1];
+        f[0] = true;
+        for (int i=1; i<len+1; i++)
+            for (int j=0; j<i; j++)
+                if (f[j] && wordDict.contains(s.substring(j,i)))
+                {
+                    f[i] = true;
+                    break;
+                }
+        return f[len];
+    }
+}
